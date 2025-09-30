1e99400afc3e32a1e7371558b25fd45c0d944a26
Gargi Gupta <142432424+GargiGuptaaaa@users.noreply.github.com>
2025-09-30 23:11:56 +0530

    Update ai.service.js

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -1,7 +1,7 @@
 const {GoogleGenerativeAI} = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
 const model = genAI.getGenerativeModel({
-    model:"gemini-2.0-flash",
+    model:"gemini-2.5-flash",
     systemInstruction:`
                 Here’s a  solid system instruction for your AI code reviewer:
 
@@ -87,4 +87,5 @@ async function generateContent(prompt){
 }
  
 // console.log(result.response.text());
+
 module.exports = generateContent
